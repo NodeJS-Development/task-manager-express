2b18def2aa931031ee19acbd0f74cb0a85dfb3f7
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-25 17:28:43 -0600

    insert several documents into db

--- mongodb.js
+++ mongodb.js
@@ -20,9 +20,57 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-      name: 'Brayan',
-      age: 27,
-    });
+    // db.collection('users').insertOne({
+    //   name: 'Brayan',
+    //   age: 27,
+    // }, (error, result) => {
+    //   if (error) {
+    //     return console.log('Unable to insert user');
+    //   }
+
+    //   console.log(result.ops);
+    // });
+
+    // db.collection('users').insertMany([
+    //   {
+    //     name: 'Jen',
+    //     age: 28
+    //   },
+    //   {
+    //     name: 'Gunther',
+    //     age: 27,
+    //   },
+    // ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log('Unable to insert documents!');
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    db.collection('tasks').insertMany([
+      {
+        description: 'Get milk',
+        completed: true,
+      },
+      {
+        description: 'Buy a car',
+        completed: false,
+      },
+      {
+        description: 'Clean the house',
+        completed: false,
+      }
+    ],
+      (error, result) => {
+        if (error) {
+          return console.log('Unable to insert documents');
+        }
+
+        console.log(result.ops);
+      }
+    )
   }
 );
