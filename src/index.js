6b0698dd91f996c38e84028cda3d9c1b5ac9bca2
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-26 15:02:28 -0600

    add delete endpoints for users and tasks

--- src/index.js
+++ src/index.js
@@ -51,6 +51,8 @@ app.get('/users/:id', async (req, res) => {
 
 });
 
+// UPDATE
+
 app.patch('/users/:id', async (req, res) => {
 
   const updates = Object.keys(req.body);
@@ -70,6 +72,20 @@ app.patch('/users/:id', async (req, res) => {
 
   } catch (e) {
     res.status(400).send();
+  }
+});
+
+// DELETE
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) return res.status(404).send();
+
+    res.send(user);
+
+  } catch (error) {
+    res.status(500).send();
   }
 });
 
@@ -137,6 +153,19 @@ app.patch('/tasks/:id', async (req, res) => {
   }
 });
 
+// DELETE
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+
+    if (!task) return res.status(404).send();
+
+    res.send(task);
+
+  } catch (error) {
+    res.status(500).send();
+  }
+});
 
 // Express setup
 app.listen(port, () => {
