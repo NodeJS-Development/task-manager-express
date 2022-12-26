09f952651eb621577637d95122a72582f258bb12
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-25 18:44:20 -0600

    understand read, update and delete methods in mongodb

--- mongodb.js
+++ mongodb.js
@@ -6,15 +6,15 @@
 // access to connect to the db to perform CRUD operations
 // const MongoClient = mongodb.MongoClient;
 
-const {MongoClient, ObjectID} = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-const id = new ObjectID();
-console.log(id.id.length);
-console.log(id.toHexString().length);
+// const id = new ObjectID();
+// console.log(id.id.length);
+// console.log(id.toHexString().length);
 
 MongoClient.connect(
   connectionURL,
@@ -27,6 +27,38 @@ MongoClient.connect(
     }
 
     const db = client.db(databaseName);
+
+    // READ
+
+    // With findOne, if there are many documents that match, you always get the first one
+    // db.collection('users').findOne({ _id: ObjectID('63a8dec5640c3b76e7c57ff0') }, (error, user) => {
+    //   if (error) {
+    //     return console.log('Unable to fetch');
+    //   }
+
+    //   console.log(user);
+    // });
+
+    // db.collection('users').find({age: 27}).toArray((error, users) => {
+    //   console.log(users);
+    // });
+
+    // db.collection('users').find({age: 27}).count((error, count) => {
+    //   console.log(count);
+    // });
+
+    // Challenge
+    // db.collection('tasks').findOne({ _id: ObjectID('63a8de85ad02a0766d121d28') }, (error, task) => {
+    //   if (error) {
+    //     console.log('Unable to fetch');
+    //   }
+
+    //   console.log(task);
+    // });
+
+    // const tasks = await db.collection('tasks').find({ completed: false }).toArray();
+    // console.log(tasks);
+    // CREATE
 
     // db.collection('users').insertOne({
     //   _id: id,
@@ -81,5 +113,53 @@ MongoClient.connect(
     //     console.log(result.ops);
     //   }
     // )
+
+    // UPDATE
+
+    // db.collection('users').updateOne(
+    //   {
+    //     _id: ObjectID('63a8a94c6960c16fa77f0d2e'),
+    //   },
+    //   {
+    //     $inc: {
+    //       age: 1,
+    //     },
+    //   }
+    // ).then((result) => {
+    //   console.log(result);
+    // }).catch((error) => {
+    //   console.log(error);
+    // });
+
+    // db.collection('tasks').updateMany(
+    //   {
+    //     completed: true,
+    //   },
+    //   {
+    //     $set: {
+    //       completed: false,
+    //     },
+    //   },
+    // )
+    //   .then(console.log)
+    //   .catch(console.log);
+
+    // DELETE
+    // db.collection('users').deleteMany(
+    //   {
+    //     age: 27,
+    //   }
+    // )
+    //   .then(console.log)
+    //   .catch(console.log);
+
+    db.collection('tasks').deleteOne(
+      {
+        description: 'Get milk',
+      }
+    )
+      .then(console.log())
+      .catch(console.log())
+
   }
 );
