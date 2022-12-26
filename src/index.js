933881f6a78aedcc4deb6466d2b01a22d6af8783
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-26 14:55:17 -0600

    add update endpoints for users and tasks

--- src/index.js
+++ src/index.js
@@ -51,6 +51,28 @@ app.get('/users/:id', async (req, res) => {
 
 });
 
+app.patch('/users/:id', async (req, res) => {
+
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+    if (!user) return res.status(404).send();
+
+    res.send(user);
+
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
 
 // TASKS
 
@@ -93,7 +115,27 @@ app.get('/tasks/:id', async (req, res) => {
     res.status(500).send();
   }
 
-})
+});
+
+// UPDATE
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['description', 'completed'];
+  const isValidUpdate = updates.every(update => allowedUpdates.includes(update));
+
+  if (!isValidUpdate) return res.status(400).send({ error: 'Invalid updates!' });
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+    if (!task) return res.status(404).send();
+
+    res.send(task);
+
+  } catch (error) {
+    res.status(500).send();
+  }
+});
 
 
 // Express setup
