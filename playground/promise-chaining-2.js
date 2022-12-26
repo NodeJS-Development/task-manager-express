f861dc87bb1c74c7e1488f6d60ec69738f8933eb
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-26 12:56:19 -0600

    transform into async/await functions

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,10 +1,25 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('63a8f2ba12f5ebb3ccd441da')
-  .then((task) => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  })
-  .then(result => console.log(result))
+// Task.findByIdAndDelete('63a8f2ba12f5ebb3ccd441da')
+//   .then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then(result => console.log(result))
+//   .catch(e => console.log(e));
+
+const deleteTaskAndCount = async (id) => {
+  const task = await Task.findByIdAndDelete(id);
+
+  const count = await Task.countDocuments({ completed: false });
+
+  return {
+    task,
+    count,
+  };
+}
+
+deleteTaskAndCount('63a93390faf3bfe775b469e9')
+  .then((count) => console.log(count))
   .catch(e => console.log(e));
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -2,14 +2,31 @@ require('../src/db/mongoose');
 const User = require('../src/models/user');
 
 // 63a9192775b72c038e7c8f8d
-User.findByIdAndUpdate('63a9242a5e9fddb9363bf7f6', {
-  age: 1,
-})
-  .then(user => {
-    console.log(user);
-    return User.countDocuments({ age: 1 });
+// User.findByIdAndUpdate('63a9242a5e9fddb9363bf7f6', {
+//   age: 1,
+// })
+//   .then(user => {
+//     console.log(user);
+//     return User.countDocuments({ age: 1 });
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch(e => console.log(e));
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+
+  const count = await User.countDocuments({ age });
+
+  return {
+    user,
+    count,
+  }
+}
+
+updateAgeAndCount('63a9242a5e9fddb9363bf7f6', 27)
+  .then((response) => {
+    console.log('response', response);
   })
-  .then((result) => {
-    console.log(result);
-  })
-  .catch(e => console.log(e));
+  .catch((e) => console.log(e));
