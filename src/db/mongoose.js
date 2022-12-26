5f7390b7728aa2c07430d452292fbf325a7389a3
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-25 23:29:49 -0600

    clean up mongoose file to handle mongoose setup only

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,43 +6,14 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   autoIndex: true,
 });
 
-const User = mongoose.model(
-  'User',
-  {
-    name: {
-      type: String,
-    },
-    age: {
-      type: Number,
-    }
-  }
-);
-
-// const me = new User({
-//   name: 'Brayan',
-//   age: 'Mike',
-// });
-
-// me.save().then(() => {
-//   console.log(me);
-// }).catch((error) => {
-//   console.log(error);
-// });
-
 const Task = mongoose.model('Task', {
   description: {
     type: String,
+    required: true,
+    trim: true,
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false,
   },
 });
-
-const task = new Task({
-  description: 'Study Node.js',
-  completed: false,
-});
-
-task.save().then(() => {
-  console.log(task);
-}).catch((error) => console.log(error));
