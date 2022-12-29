ed59f78d8fbbc87fcd9a9874dff8137fad78c2e2
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-29 15:53:35 -0600

    add functionality to delete all tasks associated to a user

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -95,6 +96,8 @@ userSchema.statics.findByCredentials = async (email, password) => {
 };
 
 
+// Middlewares
+
 // fn has to be a standart fn and not arrow functions
 // Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
@@ -103,6 +106,14 @@ userSchema.pre('save', async function (next) {
   if (user.isModified('password')) {
     user.password = await bcrypt.hash(user.password, 8);
   }
+
+  next();
+});
+
+// Delete User tasks when user is removed
+userSchema.pre('remove', async function (next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
 
   next();
 });
