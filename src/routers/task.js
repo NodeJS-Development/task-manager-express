c0f49a10756561f36a0f220765399c7f3ffa7644
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-29 16:42:12 -0600

    add pagination and sorting

--- src/routers/task.js
+++ src/routers/task.js
@@ -23,18 +23,31 @@ router.post('/tasks', auth, async (req, res) => {
 // READ
 
 // GET /tasks?completed=true/false
+// GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt_desc/createdAt_asc
 router.get('/tasks', auth, async (req, res) => {
   const match = {};
+  const sort = {};
 
   if (req.query.completed) {
     match.completed = req.query.completed === 'true'
-  } 
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split('_');
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1; 
+  }
 
   try {
     // const tasks = await Task.find({ owner: req.user._id });
     await req.user.populate({
       path: 'tasks',
       match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort,
+      }
     });
     res.send(req.user.tasks);
   } catch (e) {
