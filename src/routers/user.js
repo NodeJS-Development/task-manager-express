f6e21c0fe8865f88fc379a25734ccb57b01151e9
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-30 01:49:02 -0600

    change img type. use sharp to resize image and also convert it to png

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 
@@ -131,7 +132,8 @@ const upload = multer({
 });
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-  req.user.avatar = req.file.buffer; // this is available when the 'dest' option is not setup on our multer (upload constant in this case)
+  const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+  req.user.avatar = buffer; // this is available when the 'dest' option is not setup on our multer (upload constant in this case)
   await req.user.save();
   res.send();
 }, (error, req, res, next) => {
@@ -156,7 +158,7 @@ router.get('/users/:id/avatar', async (req, res) => {
       throw new Error();
     }
 
-    res.set('Content-Type', 'image/jpg');
+    res.set('Content-Type', 'image/png');
     res.send(user.avatar);
 
   } catch (e) {
