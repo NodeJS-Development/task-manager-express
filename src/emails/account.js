74df01e7423b805755a3316c8c583c15fefc262f
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-30 11:13:24 -0600

    refactor code to not expose sg api key and create and export sendWelcomeEmail fn

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,6 +1,6 @@
 const sgMail = require('@sendgrid/mail');
 
-const sendgridAPI = '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';
+const sendgridAPI = process.env.SENDGRID_API_KEY;
 
 sgMail.setApiKey(sendgridAPI);
 
@@ -11,3 +11,15 @@ sgMail.send({
   text: 'I hope this one actually get to you',
 });
 
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: email,
+    subject: `Thanks for joining in!`,
+    text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+  })
+};
+
+module.exports = {
+  sendWelcomeEmail,
+}
